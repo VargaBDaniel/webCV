c870879bec9c9a5911a4a68137cef7a250e56aec
Daniel Varga <daxendor13@gmail.com>
2020-05-20 15:03:59 +0200

    v0.2.2

--- scripts/changeBio.js
+++ scripts/changeBio.js
@@ -12,7 +12,25 @@ function getBioPage() {
 }
 
 function changeBioPage(bioPageToLoad) {
+    //load bio page
     let bioFrame = document.getElementById("bio-frame");
-    
     bioFrame.setAttribute("src", bioPageToLoad + ".html");
+
+    //activate any previously deactivated cards
+    let cards = document.getElementsByClassName("card");
+    let cardsArray = Object.values(cards);
+    cardsArray.forEach(card => {
+        card.classList.remove("deactivated");
+        if (card.hasAttribute("onclick")) {
+            
+        } else {
+            card.setAttribute("onclick", "getBioPage()");
+        }
+    });
+
+    //deactivate clicked card
+    let clickedElement = document.getElementById(bioPageToLoad);
+    clickedElement.classList.add("deactivated");
+
+    clickedElement.removeAttribute("onclick");
 }
